f3a3cd0f1c241ddd63d3eec3754067362e6f982d
Veniamin Albaev <albenik@gmail.com>
2018-06-12 20:55:54 +0300

    Allow skip Read or Write in fakerw.IO

--- fakerw.go
+++ fakerw.go
@@ -15,17 +15,14 @@ type IO struct {
 type FakeReadWriter struct {
 	active   bool
 	sequence []*IO
-	r        io.Reader
-	w        io.Writer
-	lvl      int
+	opnum    int
 	rc       int
 	wc       int
 	err      error
 }
 
 func NewFakeReadWriter(a bool, s ...*IO) *FakeReadWriter {
-	op, s := s[0], s[1:]
-	return &FakeReadWriter{active: a, sequence: s, r: op.R, w: op.W, lvl: 1}
+	return &FakeReadWriter{active: a, sequence: s, opnum: 1}
 }
 
 func (rw *FakeReadWriter) Read(p []byte) (int, error) {
@@ -33,35 +30,37 @@ func (rw *FakeReadWriter) Read(p []byte) (int, error) {
 		return 0, rw.err
 	}
 
+	if rw.opnum > len(rw.sequence) {
+		rw.err = &IOError{message: fmt.Sprintf("unexpected read %d bytes @ %d:r%d/w%d", len(p), rw.opnum, rw.rc, rw.wc)}
+		return 0, rw.err
+	}
+	op := rw.sequence[rw.opnum-1]
+
 	rw.rc++
 
-	if rw.r == nil {
-		rw.err = &IOError{message: fmt.Sprintf("unexpected read %d bytes @ %d:r%d/w%d", len(p), rw.lvl, rw.rc, rw.wc)}
+	if op.R == nil {
+		rw.err = &IOError{message: fmt.Sprintf("unexpected read %d bytes @ %d:r%d/w%d", len(p), rw.opnum, rw.rc, rw.wc)}
 		return 0, rw.err
 	}
 
-	if rw.active && rw.wc == 0 {
-		rw.err = &IOError{message: fmt.Sprintf("unexpected read %d bytes before write @ %d:r%d/w%d", len(p), rw.lvl, rw.rc, rw.wc)}
+	if rw.active && rw.wc == 0 && op.W != nil {
+		rw.err = &IOError{message: fmt.Sprintf("unexpected read %d bytes before write @ %d:r%d/w%d", len(p), rw.opnum, rw.rc, rw.wc)}
 		return 0, rw.err
 	}
 
-	n, err := rw.r.Read(p)
+	n, err := op.R.Read(p)
 
 	if err != nil && err != ErrRepeat {
 		if ioerr, ok := err.(*IOError); ok {
-			rw.err = &IOError{message: fmt.Sprintf("read %d bytes error @ %d:r%d/w%d (%s)", len(p), rw.lvl, rw.rc, rw.wc, ioerr.message)}
+			rw.err = &IOError{message: fmt.Sprintf("read %d bytes error @ %d:r%d/w%d (%s)", len(p), rw.opnum, rw.rc, rw.wc, ioerr.message)}
 			return 0, rw.err
 		}
 		return n, err
 	}
 
 	if err != ErrRepeat {
-		rw.r = nil
-		if rw.active && len(rw.sequence) > 0 {
-			var op *IO
-			op, rw.sequence = rw.sequence[0], rw.sequence[1:]
-			rw.r, rw.w = op.R, op.W
-			rw.lvl++
+		if rw.active || op.W == nil {
+			rw.opnum++
 			rw.rc = 0
 			rw.wc = 0
 		}
@@ -75,35 +74,37 @@ func (rw *FakeReadWriter) Write(p []byte) (int, error) {
 		return 0, rw.err
 	}
 
+	if rw.opnum > len(rw.sequence) {
+		rw.err = &IOError{message: fmt.Sprintf("unexpected write [% X] @ %d:r%d/w%d", p, rw.opnum, rw.rc, rw.wc)}
+		return 0, rw.err
+	}
+	op := rw.sequence[rw.opnum-1]
+
 	rw.wc++
 
-	if rw.w == nil {
-		rw.err = &IOError{message: fmt.Sprintf("unexpected write [% X] @ %d:r%d/w%d", p, rw.lvl, rw.rc, rw.wc)}
+	if op.W == nil {
+		rw.err = &IOError{message: fmt.Sprintf("unexpected write [% X] @ %d:r%d/w%d", p, rw.opnum, rw.rc, rw.wc)}
 		return 0, rw.err
 	}
 
-	if !rw.active && rw.rc == 0 {
-		rw.err = &IOError{message: fmt.Sprintf("unexpected write [% X] before read @ %d:r%d/w%d", p, rw.lvl, rw.rc, rw.wc)}
+	if !rw.active && rw.rc == 0 && op.R != nil {
+		rw.err = &IOError{message: fmt.Sprintf("unexpected write [% X] before read @ %d:r%d/w%d", p, rw.opnum, rw.rc, rw.wc)}
 		return 0, rw.err
 	}
 
-	n, err := rw.w.Write(p)
+	n, err := op.W.Write(p)
 
 	if err != nil && err != ErrRepeat {
 		if ioerr, ok := err.(*IOError); ok {
-			rw.err = &IOError{message: fmt.Sprintf("write [% X] error @ %d:r%d/w%d (%s)", p, rw.lvl, rw.rc, rw.wc, ioerr.message)}
+			rw.err = &IOError{message: fmt.Sprintf("write [% X] error @ %d:r%d/w%d (%s)", p, rw.opnum, rw.rc, rw.wc, ioerr.message)}
 			return 0, rw.err
 		}
 		return n, err
 	}
 
 	if err != ErrRepeat {
-		rw.w = nil
-		if !rw.active && len(rw.sequence) > 0 {
-			var op *IO
-			op, rw.sequence = rw.sequence[0], rw.sequence[1:]
-			rw.r, rw.w = op.R, op.W
-			rw.lvl++
+		if !rw.active || op.R == nil {
+			rw.opnum++
 			rw.rc = 0
 			rw.wc = 0
 		}
